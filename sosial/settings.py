fa728d627e8309caf3626746b74ad437cba8b243
vahid <vahidtwo@gmail.com>
2020-02-03 19:24:47 +0330

    try to fix CORS error

--- sosial/settings.py
+++ sosial/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -27,72 +26,71 @@ DEBUG = True
 
 # Application definition
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
+	'DEFAULT_AUTHENTICATION_CLASSES': [
+		'rest_framework_simplejwt.authentication.JWTAuthentication',
+	],
 }
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'accounts.apps.AccountsConfig',
-    'posts.apps.PostsConfig',
-    'chanel.apps.ChanelConfig',
-    'comment.apps.CommentConfig',
-    'like.apps.LikeConfig',
-    'notify.apps.NotifyConfig',
-    'files.apps.FilesConfig',
-    'rest_framework',
-    'corsheaders',
+	'django.contrib.admin',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
+	'accounts.apps.AccountsConfig',
+	'posts.apps.PostsConfig',
+	'chanel.apps.ChanelConfig',
+	'comment.apps.CommentConfig',
+	'like.apps.LikeConfig',
+	'notify.apps.NotifyConfig',
+	'files.apps.FilesConfig',
+	'rest_framework',
+	'corsheaders',
 ]
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
+	'corsheaders.middleware.CorsMiddleware',
+
+	'django.middleware.security.SecurityMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'sosial.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
+	{
+		'BACKEND': 'django.template.backends.django.DjangoTemplates',
+		'DIRS': [],
+		'APP_DIRS': True,
+		'OPTIONS': {
+			'context_processors': [
+				'django.template.context_processors.debug',
+				'django.template.context_processors.request',
+				'django.contrib.auth.context_processors.auth',
+				'django.contrib.messages.context_processors.messages',
+			],
+		},
+	},
 ]
 
 WSGI_APPLICATION = 'sosial.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 #
 AUTH_USER_MODEL = 'accounts.User'
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(days=360),
-    'ROTATE_REFRESH_TOKENS': True,
-    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=365),
-    'SLIDING_TOKEN_LIFETIME': datetime.timedelta(days=360),
-    'SLIDING_TOKEN_REFRESH_LIFETIME': datetime.timedelta(days=365),
+	'ACCESS_TOKEN_LIFETIME': datetime.timedelta(days=360),
+	'ROTATE_REFRESH_TOKENS': True,
+	'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=365),
+	'SLIDING_TOKEN_LIFETIME': datetime.timedelta(days=360),
+	'SLIDING_TOKEN_REFRESH_LIFETIME': datetime.timedelta(days=365),
 }
 DATABASES = {
     'default': {
@@ -101,14 +99,14 @@ DATABASES = {
     }
 }
 # DATABASES = {
-#      'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'social',
-#         'USER': 'root',
-#         'PASSWORD': '123',
-#         'HOST': '127.0.0.1',
-#         'PORT': '3306',
-#     }
+# 	'default': {
+# 		'ENGINE': 'django.db.backends.mysql',
+# 		'NAME': 'social',
+# 		'USER': 'root',
+# 		'PASSWORD': '123',
+# 		'HOST': '127.0.0.1',
+# 		'PORT': '3306',
+# 	}
 # }
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -118,20 +116,19 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+	{
+		'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+	},
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
@@ -145,7 +142,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
